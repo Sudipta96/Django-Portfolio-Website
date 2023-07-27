71a1e37e9934db12d666b40b3378d776c402909a
sudipta-sarker <sudiptasarker583@gmail.com>
2023-07-27 20:29:40 +0600

    6th - db conf changed again

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 # build_files.sh
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# python3.9 manage.py migrate 
+# python3.9 manage.py collectstatic
+
+
+#new try
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -85,32 +85,32 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
-
-# if DEBUG == "True":
-#     DATABASES = {
+# DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.sqlite3',
 #             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #         }
 #     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ.get("DB_NAME"),
-#             'USER': os.environ.get("DB_USER"),
-#             'PASSWORD': os.environ.get("DB_PASS"),
-#             'HOST': os.environ.get("DB_HOST"),
-#             'PORT': os.environ.get("DB_PORT"),
-#         }
-#     }
+
+
+if DEBUG == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("DB_NAME"),
+            'USER': os.environ.get("DB_USER"),
+            'PASSWORD': os.environ.get("DB_PASS"),
+            'HOST': os.environ.get("DB_HOST"),
+            'PORT': os.environ.get("DB_PORT"),
+        }
+    }
 
 
 # Password validation
