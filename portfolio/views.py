8aa96927c3171b8e6ac7fd93620197da36eedc86
sudipta-sarker <sudiptasarker583@gmail.com>
2023-07-27 23:58:12 +0600

    p - 10 create super user - try-1

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,6 +5,19 @@ from django.contrib import messages
 # Create your views here.
 
 
+from django.contrib.auth.models import User
+
+username = 'admin'
+email = 'admin@example.com'
+password = 'super_password'
+
+if User.objects.filter(username=username).count() == 0:
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created.')
+else:
+    print('Superuser creation failed. User already exists.')
+
+
 def index(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
