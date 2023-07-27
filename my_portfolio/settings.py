fbe5c073c4fede01984487007280bab275112025
sudipta-sarker <sudiptasarker583@gmail.com>
2023-07-27 19:36:26 +0600

    5th commit - database conf changed

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -84,24 +84,33 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG == "True":
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get("DB_NAME"),
-            'USER': os.environ.get("DB_USER"),
-            'PASSWORD': os.environ.get("DB_PASS"),
-            'HOST': os.environ.get("DB_HOST"),
-            'PORT': os.environ.get("DB_PORT"),
-        }
-    }
+
+
+# if DEBUG == "True":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': os.environ.get("DB_NAME"),
+#             'USER': os.environ.get("DB_USER"),
+#             'PASSWORD': os.environ.get("DB_PASS"),
+#             'HOST': os.environ.get("DB_HOST"),
+#             'PORT': os.environ.get("DB_PORT"),
+#         }
+#     }
 
 
 # Password validation
