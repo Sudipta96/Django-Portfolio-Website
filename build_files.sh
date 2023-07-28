22a65900265cd0a44470544e2fc4a84d688c1380
sudipta-sarker <sudiptasarker583@gmail.com>
2023-07-28 17:24:09 +0600

    p-22 try once again

--- build_files.sh
+++ build_files.sh
@@ -28,6 +28,6 @@ pip install -r requirements.txt
 
 # 9. Run Django migrations and collect static files
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic --no-input
+python3.9 manage.py collectstatic --no-input --clear
 
 echo "Setup complete"
