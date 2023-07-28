3bec848f9589c53c67218f12913fe84ee99d0dad
sudipta-sarker <sudiptasarker583@gmail.com>
2023-07-28 16:05:20 +0600

    p-19 change static root directory

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -162,7 +162,5 @@ if DEBUG == "True":
     os.path.join(BASE_DIR, 'static'),
 ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-
-# STATIC_ROOT = os.path.join('BASE_DIR', 'static')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+    STATIC_ROOT = os.path.join('BASE_DIR', 'static')
