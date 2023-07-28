17585fd3b8a3f6fd7fddc0b5e6ec7a6765206e57
sudipta-sarker <sudiptasarker583@gmail.com>
2023-07-28 17:17:41 +0600

    p-22 another one

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -157,11 +157,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# if DEBUG == "True":
-#     STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-# else:
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG == "True":
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
